f0181c55e4ad614d1081c9e310d1e07c711cc8f9
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-07-26 16:59:46 -0500

    Add mobile menu functionality

--- js/main.js
+++ js/main.js
@@ -1,18 +1,49 @@
+const div = document.createElement('div');
 const hamBttn = document.querySelector('.menu');
 const mainpage = document.querySelector('.main-page');
 const header = document.querySelector('header');
 const worksSection = document.querySelector('.works-section');
-
 const blurElements = [mainpage, header, worksSection];
 
-hamBttn.addEventListener('click', () => {
-  const div = document.createElement('div');
+hamBttn.addEventListener('click', function() {
+  // Create div for mobile menu
   div.id = 'overlay';
+  div.innerHTML = 
+  '<img src="/Images/cancel.png" class="cancelBttn"></img>'+
+  '<ul class="mobile-menu-ul">'+
+    '<li class="mobile-menu-li" id="1">Porfolio</li>'+
+    '<li class="mobile-menu-li" id="2">About</li>'+
+    '<li class="mobile-menu-li" id="3">Contact</li>'+
+  '</ul>';
   document.body.appendChild(div);
   div.style.display = 'block';
-
-  // Modify main-page css
+  // Add css class to behind sections
   for (let i = 0; i < blurElements.length; i += 1) {
     blurElements[i].classList.add('blur-content');
   }
-});
+  // Add listener for closing menu
+  const mobileMenu = document.querySelector(".cancelBttn");
+  mobileMenu.addEventListener('click', function(){
+    div.remove();
+    UnBlurSections();
+  })
+  // Adding listeners to li tags
+  const sections = document.querySelectorAll(".mobile-menu-li");
+  for (i = 0; i < sections.length; i += 1) {
+    sections[i].addEventListener('click', function(event){
+      div.remove();
+      UnBlurSections();
+      window.location.hash = "section-"+event.target.id;
+    });
+  }
+})
+
+function UnBlurSections(){
+  for (let i = 0; i < blurElements.length; i += 1) {
+    blurElements[i].classList.remove('blur-content');
+  }
+}
+
+
+
+
