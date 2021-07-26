02a421c5a1cee7604a871ba3f0760dafb7248a8f
Laylooo <khodjaeva.laylo@gmail.com>
2021-07-27 03:13:51 +0500

    Linter checks

--- js/main.js
+++ js/main.js
@@ -5,16 +5,20 @@ const header = document.querySelector('header');
 const worksSection = document.querySelector('.works-section');
 const blurElements = [mainpage, header, worksSection];
 
-hamBttn.addEventListener('click', function() {
+function UnBlurSections() {
+  for (let i = 0; i < blurElements.length; i += 1) {
+    blurElements[i].classList.remove('blur-content');
+  }
+}
+hamBttn.addEventListener('click', () => {
   // Create div for mobile menu
   div.id = 'overlay';
-  div.innerHTML = 
-  '<img src="/Images/cancel.png" class="cancelBttn"></img>'+
-  '<ul class="mobile-menu-ul">'+
-    '<li class="mobile-menu-li" id="1">Porfolio</li>'+
-    '<li class="mobile-menu-li" id="2">About</li>'+
-    '<li class="mobile-menu-li" id="3">Contact</li>'+
-  '</ul>';
+  div.innerHTML = '<img src="/Images/cancel.png" class="cancelBttn"></img>'
+    + '<ul class="mobile-menu-ul">'
+    + '<li class="mobile-menu-li" id="1">Porfolio</li>'
+    + '<li class="mobile-menu-li" id="2">About</li>'
+    + '<li class="mobile-menu-li" id="3">Contact</li>'
+    + '</ul>';
   document.body.appendChild(div);
   div.style.display = 'block';
   // Add css class to behind sections
@@ -22,28 +26,18 @@ hamBttn.addEventListener('click', function() {
     blurElements[i].classList.add('blur-content');
   }
   // Add listener for closing menu
-  const mobileMenu = document.querySelector(".cancelBttn");
-  mobileMenu.addEventListener('click', function(){
+  const mobileMenu = document.querySelector('.cancelBttn');
+  mobileMenu.addEventListener('click', () => {
     div.remove();
     UnBlurSections();
-  })
+  });
   // Adding listeners to li tags
-  const sections = document.querySelectorAll(".mobile-menu-li");
-  for (i = 0; i < sections.length; i += 1) {
-    sections[i].addEventListener('click', function(event){
+  const sections = document.querySelectorAll('.mobile-menu-li');
+  for (let i = 0; i < sections.length; i += 1) {
+    sections[i].addEventListener('click', (event) => {
       div.remove();
       UnBlurSections();
-      window.location.hash = "section-"+event.target.id;
+      window.location.hash = `section-${event.target.id}`;
     });
   }
-})
-
-function UnBlurSections(){
-  for (let i = 0; i < blurElements.length; i += 1) {
-    blurElements[i].classList.remove('blur-content');
-  }
-}
-
-
-
-
+});
